24402ef4e7e1964809524ae7ada51799520e743b
Conrad Ludgate <conradludgate@gmail.com>
2020-11-25 12:03:42 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/conradludgate/bors-test-repo
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func bifurcate() {
+	fmt.Println("bifurcate called")
+}
+
+func main() {
+	bifurcate()
+
+	fmt.Println("hello")
+}
